7cbda3a9a7005c58e2ac71ea0e5bfdd74fdbc93e
Katrin Rudisch <Katrin Rudisch@Katrin.wireless.hhs.nl>
2014-11-28 14:41:33 +0100

    test

---
+++ TicTacToe/src/kb50/tictactoe/MainActivity.java
@@ -0,0 +1,35 @@
+package kb50.tictactoe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+    public void onClick(View v){
+    	switch(v.getId()){
+    	case R.id.btn_oneplayer:
+    		Intent single = new Intent("kb50.tictactoe.ChooseDifficulty");
+    		startActivity(single);
+    		break;
+    	
+    	case R.id.btn_twoplayer:
+    		/*Intent multi = new Intent(this, MultiPlayer.class);
+    		startActivity(multi);*/
+    		break;
+    		
+    	}
+    	
+    	
+    	
+    }
+}
