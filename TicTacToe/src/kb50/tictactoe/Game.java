eaee791c5bf8cb3589cca272a001a165fa1b62d1
11021896 <11021896@student.hhs.nl>
2014-12-01 15:56:49 +0100

    turn added

--- TicTacToe/src/kb50/tictactoe/Game.java
+++ TicTacToe/src/kb50/tictactoe/Game.java
@@ -1,5 +1,7 @@
 package kb50.tictactoe;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -8,15 +10,72 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Game extends Activity {
 	private boolean turn;
+	private boolean multiplayer;
+	private String Player1;
+	private String Player2;
+	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        turn = true;
+        setBaseVar();
+    }
+
+    
+    private void setBaseVar(){
+    	try{
+    		String a = getIntent().getStringExtra("Player");
+    		if (a.equals("two")){
+
+    	    	Player1 = getIntent().getStringExtra("p1_name");
+    			Player2 = getIntent().getStringExtra("p2_name");
+    				if (Player1.equals("") || Player2.equals("")){
+    					//something went wrong
+    				}
+    				else{
+    					multiplayer = true;
+    					int ran = new Random().nextInt(2);
+    					
+    					if(ran == 1){
+    						turn = true;
+
+    			        	TextView t1 = (TextView) findViewById(R.id.turn);
+    						t1.setText("It is "+Player1+" 's turn");
+    			    		
+    					}
+    					else{
+    						turn = false;
+
+    			        	TextView t1 = (TextView) findViewById(R.id.turn);
+
+    						t1.setText("It is "+Player2+" 's turn");
+    			    		
+    					}
+    				
+    				}
+
+    		}
+    		else{
+    				multiplayer = false;
+    				TextView t1 = (TextView) findViewById(R.id.turn);
+    				t1.setText("It is your turn");
+
+    		}
+    		
+    	}
+    	catch(Exception e){
+    		
+    	}
+    	
+    	
+    	
+			
+	    
     }
     
     public void onClickBtn(View v){
@@ -31,23 +90,72 @@ public class Game extends Activity {
 			startActivity(k);
 			break;
     	default: 
-    		ImageButton btn = (ImageButton) findViewById(v.getId());
-        	
-        	Resources res = getResources();
-        	Drawable cross = res.getDrawable(R.drawable.candycane);
-        	Drawable circle = res.getDrawable(R.drawable.cookie);
-        	
-        	if(turn == true){
-        		btn.setImageDrawable(cross);
-        		btn.setClickable(false);
-        		turn = false;
-        	}else{
-        		btn.setImageDrawable(circle);
-        		btn.setClickable(false);
-        		turn = true;
-        	}
-        	break;
+    		if(multiplayer == false){
+    			onePlayer(v);
+    		}
+    		else{
+    			twoPlayers(v);
+    		}
+    		break;
 
     	}
     }
+
+    
+    private void onePlayer(View v){
+    	ImageButton btn = (ImageButton) findViewById(v.getId());
+    	
+    	Resources res = getResources();
+    	Drawable cross = res.getDrawable(R.drawable.candycane);
+    	Drawable circle = res.getDrawable(R.drawable.cookie);
+    	
+    	if(turn == true){
+    		btn.setImageDrawable(cross);
+    		btn.setClickable(false);
+
+
+        	TextView t1 = (TextView) findViewById(R.id.turn);
+        	t1.setText("It is Your turn");
+    		
+    		turn = false;
+    	}else{
+    		btn.setImageDrawable(circle);
+    		btn.setClickable(false);
+    		
+    		//computer action
+    		
+    		turn = true;
+    	}
+
+    }
+
+    private void twoPlayers(View v){
+    	ImageButton btn = (ImageButton) findViewById(v.getId());
+    	
+    	Resources res = getResources();
+    	Drawable cross = res.getDrawable(R.drawable.candycane);
+    	Drawable circle = res.getDrawable(R.drawable.cookie);
+    	
+    	if(turn == true){
+    		btn.setImageDrawable(cross);
+    		btn.setClickable(false);
+
+        	TextView t1 = (TextView) findViewById(R.id.turn);
+        	t1.setText("It is "+Player1+" 's turn");
+    		
+    		turn = false;
+    	}else{
+    		btn.setImageDrawable(circle);
+    		btn.setClickable(false);
+    		
+
+        	TextView t1 = (TextView) findViewById(R.id.turn);
+        	t1.setText("It is "+Player2+" 's turn");
+    		
+    		turn = true;
+    	}
+    } 
+    
+    
+    
 }
--- TicTacToe/src/kb50/tictactoe/MultiPlayer.java
+++ TicTacToe/src/kb50/tictactoe/MultiPlayer.java
@@ -1,12 +1,14 @@
 package kb50.tictactoe;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MultiPlayer extends Activity {
 
@@ -27,17 +29,28 @@ public class MultiPlayer extends Activity {
 		case R.id.btn_play:
 			Intent play = new Intent(this, Game.class);
 			
-			/*
 			String nameP1 = inputP1.getText().toString(); 
 			
 			String nameP2 = inputP2.getText().toString();
 			
-			play.putExtra("p1_name", nameP1);
-			play.putExtra("p2_name", nameP2);
+			if(nameP1.equals("") || nameP2.equals("")){
+				Context context = getApplicationContext();
+				CharSequence text = "Fill in the names";
+				int duration = Toast.LENGTH_SHORT;
+
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.show();
+
+			}else{
+				play.putExtra("p1_name", nameP1);
+				play.putExtra("p2_name", nameP2);
+
+				play.putExtra("Player", "two");
+				
+//				startActivityForResult(play,1);
+				startActivity(play);
+			}
 			
-			startActivityForResult(play,1);
-			 */
-			startActivity(play);
 			break;
 			
 		case R.id.btn_back:
