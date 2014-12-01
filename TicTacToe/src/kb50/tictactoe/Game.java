f877c662f2e05c5a3f5a1dc937f42f127135b151
Tony Kwan <tonykwan93@gmail.com>
2014-12-01 16:05:29 +0100

    Check if somebody has won works

--- TicTacToe/src/kb50/tictactoe/Game.java
+++ TicTacToe/src/kb50/tictactoe/Game.java
@@ -1,21 +1,46 @@
 package kb50.tictactoe;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class Game extends Activity {
 	private boolean turn;
+	private ImageButton btn1;
+	private ImageButton btn2;
+	private ImageButton btn3;
+	private ImageButton btn4;
+	private ImageButton btn5;
+	private ImageButton btn6;
+	private ImageButton btn7;
+	private ImageButton btn8;
+	private ImageButton btn9;
+	private Resources res;
+	private Drawable cross;
+	private Drawable circle;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        btn1 = (ImageButton) findViewById(R.id.ib_1);
+        btn2 = (ImageButton) findViewById(R.id.ib_2);
+        btn3 = (ImageButton) findViewById(R.id.ib_3);
+        btn4 = (ImageButton) findViewById(R.id.ib_4);
+        btn5 = (ImageButton) findViewById(R.id.ib_5);
+        btn6 = (ImageButton) findViewById(R.id.ib_6);
+        btn7 = (ImageButton) findViewById(R.id.ib_7);
+        btn8 = (ImageButton) findViewById(R.id.ib_8);
+        btn9 = (ImageButton) findViewById(R.id.ib_9);
+        res = getResources();
+        cross = res.getDrawable(R.drawable.candycane);
+        circle = res.getDrawable(R.drawable.cookie);
         turn = true;
     }
     
@@ -30,24 +55,270 @@ public class Game extends Activity {
 			Intent k = new Intent (this, MainActivity.class);
 			startActivity(k);
 			break;
-    	default: 
-    		ImageButton btn = (ImageButton) findViewById(v.getId());
-        	
-        	Resources res = getResources();
-        	Drawable cross = res.getDrawable(R.drawable.candycane);
-        	Drawable circle = res.getDrawable(R.drawable.cookie);
-        	
+    	case R.id.ib_1:    	        	
         	if(turn == true){
-        		btn.setImageDrawable(cross);
-        		btn.setClickable(false);
+        		btn1.setImageDrawable(cross);
+        		
+        		btn1.setTag("cross");
+        		
+        		btn1.setClickable(false);
+        		check();
         		turn = false;
         	}else{
-        		btn.setImageDrawable(circle);
-        		btn.setClickable(false);
+        		btn1.setImageDrawable(circle);
+        		
+        		btn1.setTag("circle");
+        		
+        		btn1.setClickable(false);
+        		check();
         		turn = true;
         	}
         	break;
+    	case R.id.ib_2:    	        	
+        	if(turn == true){
+        		btn2.setImageDrawable(cross);
 
+        		btn2.setTag("cross");
+        		
+        		btn2.setClickable(false);
+        		check();
+        		turn = false;
+        	}else{
+        		btn2.setImageDrawable(circle);
+        		
+        		btn2.setTag("circle");
+        		
+        		btn2.setClickable(false);
+        		check();
+        		turn = true;
+        	}
+        	break;
+    	case R.id.ib_8:    	        	
+        	if(turn == true){
+        		btn8.setImageDrawable(cross);
+        		
+        		btn8.setTag("cross");
+        		
+        		btn8.setClickable(false);
+        		check();
+        		turn = false;
+        	}else{
+        		btn8.setImageDrawable(circle);
+        		
+        		btn8.setTag("circle");
+        		
+        		btn8.setClickable(false);
+        		check();
+        		turn = true;
+        	}
+        	break;
+    	case R.id.ib_9:    	        	
+        	if(turn == true){
+        		btn9.setImageDrawable(cross);
+   
+        		btn9.setTag("cross");
+        		
+        		btn9.setClickable(false);
+        		check();
+        		turn = false;
+        	}else{
+        		btn9.setImageDrawable(circle);
+        		
+        		btn9.setTag("circle");
+        		
+        		btn9.setClickable(false);
+        		check();
+        		turn = true;
+        	}
+        	break;
+    	case R.id.ib_3:    	        	
+        	if(turn == true){
+        		btn3.setImageDrawable(cross);
+        		
+        		btn3.setTag("cross");
+        		
+        		btn3.setClickable(false);
+        		check();
+        		turn = false;
+        	}else{
+        		btn3.setImageDrawable(circle);
+        		
+        		btn3.setTag("circle");
+        		
+        		btn3.setClickable(false);
+        		check();
+        		turn = true;
+        	}
+        	break;
+    	case R.id.ib_4:    	        	
+        	if(turn == true){
+        		btn4.setImageDrawable(cross);
+        		
+        		btn4.setTag("cross");
+        		
+        		btn4.setClickable(false);
+        		check();
+        		turn = false;
+        	}else{
+        		btn4.setImageDrawable(circle);
+        		
+        		btn4.setTag("circle");
+        		
+        		btn4.setClickable(false);
+        		check();
+        		turn = true;
+        	}
+        	break;
+    	case R.id.ib_5:    	        	
+        	if(turn == true){
+        		btn5.setImageDrawable(cross);
+        		
+        		btn5.setTag("cross");
+        		
+        		btn5.setClickable(false);
+        		check();
+        		turn = false;
+        	}else{
+        		btn5.setImageDrawable(circle);
+        		
+        		btn5.setTag("circle");
+        		
+        		btn5.setClickable(false);
+        		check();
+        		turn = true;
+        	}
+        	break;
+    	case R.id.ib_6:    	        	
+        	if(turn == true){
+        		btn6.setImageDrawable(cross);
+        		
+        		btn6.setTag("cross");
+        		
+        		btn6.setClickable(false);
+        		check();
+        		turn = false;
+        	}else{
+        		btn6.setImageDrawable(circle);
+        		
+        		btn6.setTag("circle");
+        		
+        		btn6.setClickable(false);
+        		check();
+        		turn = true;
+        	}
+        	break;
+    	case R.id.ib_7:    	        	
+        	if(turn == true){
+        		btn7.setImageDrawable(cross);
+        		
+        		btn7.setTag("cross");
+        		
+        		btn7.setClickable(false);
+        		check();
+        		turn = false;
+        	}else{
+        		btn7.setImageDrawable(circle);
+        		
+        		btn7.setTag("circle");
+        		
+        		btn7.setClickable(false);
+        		check();
+        		turn = true;
+        	}
+        	break;
     	}
+    }
+    
+    private void check(){    	    	
+    	Object d1 = btn1.getTag();
+    	Object d2 = btn2.getTag();
+    	Object d3 = btn3.getTag();
+    	Object d4 = btn4.getTag();
+    	Object d5 = btn5.getTag();
+    	Object d6 = btn6.getTag();
+    	Object d7 = btn7.getTag();
+    	Object d8 = btn8.getTag();
+    	Object d9 = btn9.getTag();
+    	
+    	//Horizontal check 
+    	if(d1 != null && d2 != null && d3 != null){
+    		if(d1.equals(d2) && d2.equals(d3)){
+    			Context context = getApplicationContext();
+    			CharSequence text = "Horizontal check works";
+    			int duration = Toast.LENGTH_SHORT;
+
+    			Toast toast = Toast.makeText(context, text, duration);
+    			toast.show();
+        	}
+    	}else if(d4 != null && d5 != null && d6 != null){
+    		if(d4.equals(d5) && d5.equals(d6)){
+    			Context context = getApplicationContext();
+    			CharSequence text = "Horizontal check works";
+    			int duration = Toast.LENGTH_SHORT;
+
+    			Toast toast = Toast.makeText(context, text, duration);
+    			toast.show();
+        	}
+    	}else if(d7 != null && d8 != null && d9 != null){
+    		if(d7.equals(d8) && d8.equals(d9)){
+    			Context context = getApplicationContext();
+    			CharSequence text = "Horizontal check works";
+    			int duration = Toast.LENGTH_SHORT;
+
+    			Toast toast = Toast.makeText(context, text, duration);
+    			toast.show();
+        	}
+    	}
+    	
+    	//Vertical check
+    	if(d1 != null && d4 != null && d7 != null){
+    		if(d1.equals(d4) && d4.equals(d7)){
+    			Context context = getApplicationContext();
+    			CharSequence text = "Vertical check works";
+    			int duration = Toast.LENGTH_SHORT;
+
+    			Toast toast = Toast.makeText(context, text, duration);
+    			toast.show();
+        	}
+    	}else if(d2 != null && d5 != null && d8 != null){
+    		if(d2.equals(d5) && d5.equals(d8)){
+    			Context context = getApplicationContext();
+    			CharSequence text = "Vertical check works";
+    			int duration = Toast.LENGTH_SHORT;
+
+    			Toast toast = Toast.makeText(context, text, duration);
+    			toast.show();
+        	}
+    	}else if(d3 != null && d6 != null && d9 != null){
+    		if(d3.equals(d6) && d6.equals(d9)){
+    			Context context = getApplicationContext();
+    			CharSequence text = "Vertical check works";
+    			int duration = Toast.LENGTH_SHORT;
+
+    			Toast toast = Toast.makeText(context, text, duration);
+    			toast.show();
+        	}
+    	}    	
+    	
+    	//Diagonal check
+    	if(d1 != null && d5 != null && d9 != null){
+    		if(d1.equals(d5) && d5.equals(d9)){
+    			Context context = getApplicationContext();
+    			CharSequence text = "Diagonal check works";
+    			int duration = Toast.LENGTH_SHORT;
+
+    			Toast toast = Toast.makeText(context, text, duration);
+    			toast.show();
+        	}
+    	}else if(d3 != null && d5 != null && d7 != null){
+    		if(d3.equals(d5) && d5.equals(d7)){
+    			Context context = getApplicationContext();
+    			CharSequence text = "Diagonal check works";
+    			int duration = Toast.LENGTH_SHORT;
+
+    			Toast toast = Toast.makeText(context, text, duration);
+    			toast.show();
+        	}
+    	} 	
     }
 }
